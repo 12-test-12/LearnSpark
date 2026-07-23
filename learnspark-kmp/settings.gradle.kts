a502152079e6b4263ab289d1f397137ad6e43c76
development-and-security <1498490946@qq.com>
2026-07-23 12:34:07 +0800

    添加了linux的打包方式

--- learnspark-kmp/settings.gradle.kts
+++ learnspark-kmp/settings.gradle.kts
@@ -5,21 +5,29 @@ include(":server")
 
 pluginManagement {
     repositories {
+        // Google 官方源放最前（被 502 时回退到下面的镜像）
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+
+        // 国内镜像兜底
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/google")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
+        // Google 官方源放最前
+        google()
+        mavenCentral()
+
+        // 国内镜像兜底
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
+        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
     }
 }
